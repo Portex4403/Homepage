e1e2d4e584abd88a6e4764e753ad6a96f6649ac8
Portex4403 <145843280+Portex4403@users.noreply.github.com>
2024-03-25 13:58:59 +0100

    changed js var to let

--- Uhr.js
+++ Uhr.js
@@ -1,18 +1,18 @@
 function aktualisiereUhr(){
-    var jetzt = new Date();
-    var stunden = jetzt.getHours();
-    var minuten = jetzt.getMinutes();
-    var sekunden = jetzt.getSeconds();
+let jetzt = new Date();
+let stunden = jetzt.getHours();
+let minuten = jetzt.getMinutes();
+let sekunden = jetzt.getSeconds();
 
-    if (stunden < 10){
-        stunden = "0" + stunden;
-    }
-    if (minuten < 10){
-        minuten = "0" + minuten;
-    }
-    if (sekunden < 10){
-        sekunden = "0" + sekunden;
-    }
-    document.getElementById('Uhr').innerHTML = stunden + ":" + minuten + ":" + sekunden;
+if (stunden < 10){
+    stunden = "0" + stunden;
+}
+if (minuten < 10){
+    minuten = "0" + minuten;
+}
+if (sekunden < 10){
+    sekunden = "0" + sekunden;
+}
+document.getElementById('Uhr').innerHTML = stunden + ":" + minuten + ":" + sekunden;
 }
 setInterval(aktualisiereUhr, 1000);
